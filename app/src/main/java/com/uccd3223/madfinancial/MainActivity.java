84417858ca9a80047b343ff82eb9fdbb3f7e6087
Junhui20 <jhteoh20@1utar.my>
2024-11-30 16:43:17 +0800

    Fisrt Commit

---
+++ app/src/main/java/com/uccd3223/madfinancial/MainActivity.java
@@ -0,0 +1,14 @@
+package com.uccd3223.madfinancial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
